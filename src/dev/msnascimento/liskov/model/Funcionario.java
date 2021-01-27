256c7503fcf11a4fdecedd4bf1b97b10658ff9d4
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-26 22:08:37 -0300

    Liskov Substitution principle skiping inheritance

--- src/dev/msnascimento/liskov/model/Funcionario.java
+++ src/dev/msnascimento/liskov/model/Funcionario.java
@@ -5,54 +5,16 @@ import java.time.LocalDate;
 
 public class Funcionario {
 
-	private String nome;
-	private String cpf;
-	private Cargo cargo;
-	private BigDecimal salario;
+	private DadosPessoais dadosPessoais;
 	private LocalDate dataUltimoReajuste;
 
 	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.cargo = cargo;
-		this.salario = salario;
+		dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
 	}
 
 	public void atualizarSalario(BigDecimal salarioReajustado) {
-		this.salario = salarioReajustado;
+		this.dadosPessoais.setSalario(salarioReajustado);
 		this.dataUltimoReajuste = LocalDate.now();
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public Cargo getCargo() {
-		return cargo;
-	}
-
-	public void setCargo(Cargo cargo) {
-		this.cargo = cargo;
-	}
-
-	public BigDecimal getSalario() {
-		return salario;
-	}
-
-	public void setSalario(BigDecimal salario) {
-		this.salario = salario;
 	}
 
 	public LocalDate getDataUltimoReajuste() {
@@ -64,7 +26,15 @@ public class Funcionario {
 	}
 
 	public void promover(Cargo novoCargo) {
-		this.cargo = novoCargo;
+		this.dadosPessoais.setCargo(novoCargo);
+	}
+
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
+	}
+
+	public void setDadosPessoais(DadosPessoais dadosPessoais) {
+		this.dadosPessoais = dadosPessoais;
 	}
 
 }
